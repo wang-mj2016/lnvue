4fad881743d3351bd153cc8920adf714a2f941d9
wang-mj2016 <1418953098@qq.com>
2017-03-22 00:01:05 +0800

    星星组件完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,11 +29,24 @@
 	    <div class="background">
 	      <img :src="seller.avatar" width="100%" height="100%">
 	    </div>
-	    <div v-show="detailShow" class="detail"></div>
+	    <div v-show="detailShow" class="detail">
+	    	<div class="detail-wrapper clearfix">
+	    		<div class="detail-main">
+	    			<h1 class="name">{{seller.name}}</h1>
+	    			<div class="star-wrapper">
+	    				<star :size='48' :score='seller.score'></star>
+	    			</div>
+	    		</div>
+	    	</div>
+	    	<div class="detail-close">
+	    		<i class="icon-close"></i>
+	    	</div>
+	    </div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
+	import star from 'components/star/star';
 	export default {
 		props: {
 			seller: {
@@ -42,7 +55,7 @@
 		},
 		data() {
 			return {
-				detailShow: false
+				detailShow: true
 			};
 		},
 		methods: {
@@ -52,6 +65,9 @@
 		},
 		created() {
 			this.classMap = ['decrease','discount','special','invoice','guarantee'];
+		},
+		components: {
+			star
 		}
 	};
 </script>
@@ -199,6 +215,32 @@
     	height: 100%;
     	overflow: auto;
     	background: rgba(7,17,27,0.8);
+    	.detail-wrapper{
+    		width: 100%;
+    		min-height: 100%;
+    		.detail-main{
+    			margin-top: 64px;
+    			padding-bottom: 64px;
+    			.name{
+    				line-height: 16px;
+		            text-align: center;
+		            font-size: 16px;
+		            font-weight: 700;
+    			}
+    			.star-wrapper{
+		            margin-top: 18px;
+		            padding: 2px 0;
+		            text-align: center;
+		        }
+    		}
+    	}
+    	.detail-close{
+    		position: relative;
+    		width: 32px;
+    		height: 32px;
+    		margin: -64px auto 0;
+    		font-size: 32px;
+    	}
     }
 }
 </style>
---
+++ templata.vue
@@ -0,0 +1,11 @@
+<template>
+	
+</template>
+
+<script type="text/ecmascript-6">
+	
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+
+</style>
